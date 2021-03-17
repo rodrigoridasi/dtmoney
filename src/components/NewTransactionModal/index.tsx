8b4de7a8fac92e88746db279ede6481dce7b54cf
Rodrigo R. da Silva <rodrigoridasi@gmail.com>
2021-03-17 11:55:03 -0300

    Modal style & Button types

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
-
-import { Container } from './styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -15,25 +17,46 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             overlayClassName="react-modal-overlay"
             className="react-modal-content"
         >
+
+            <button
+                type="button"
+                onClick={onRequestClose}
+                className="react-modal-close"
+            >
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
+
             <Container>
                 <h2>Cadastrar transação</h2>
 
-                <input
-                    placeholder="Título"
-                />
+                <input placeholder="Título" />
 
                 <input
                     type="number"
                     placeholder="Valor"
                 />
 
-                <input
-                    placeholder="Categoria"
-                />
+                <TransactionTypeContainer>
+                    <button
+                        type="button"
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
 
-                <button
-                    type="submit"
-                >
+                    </button>
+
+                    <button
+                        type="button"
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+
+                    </button>
+                </TransactionTypeContainer>
+
+                <input placeholder="Categoria" />
+
+                <button type="submit">
                     Cadastrar
                 </button>
             </Container>
